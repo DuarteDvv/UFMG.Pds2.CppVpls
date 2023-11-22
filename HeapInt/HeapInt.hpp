709e3495ec11cab8348a6ed2a1907776f31ca152
Luis Antonio Duarte Sousa <136333571+DuarteDvv@users.noreply.github.com>
2023-11-21 21:59:19 -0300

    Update HeapInt.hpp

--- HeapInt/HeapInt.hpp
+++ HeapInt/HeapInt.hpp
@@ -1,1 +1,31 @@
+#ifndef _heapi
+#define _heapi
 
+class HeapInt{
+    private:
+        int* inteiro;
+
+    public:
+        HeapInt();
+        HeapInt(int &);
+        HeapInt(const HeapInt &);
+        ~HeapInt();
+
+
+        HeapInt& operator = (const HeapInt &a);
+
+        int getInt() const;
+        void setInt(int);
+
+
+        
+
+
+};
+
+
+
+
+
+
+#endif
