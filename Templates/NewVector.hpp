0a4ba553010f3f443ca553d9055634744ce503a8
Luis Antonio Duarte Sousa <136333571+DuarteDvv@users.noreply.github.com>
2023-11-22 20:59:32 -0300

    att

--- Templates/NewVector.hpp
+++ Templates/NewVector.hpp
@@ -1,1 +1,27 @@
+template <typename T>
+class Vetor{
+    private:
+        T* Vet;
 
+    public:
+
+        Vetor(int a){
+            T* A = new T[a];
+        }
+
+        Vetor(const Vetor& v){
+            int size = sizeof(v.Vet) / sizeof(v.Vet[0]);
+            Vet = new T[size];
+
+            for (int i = 0; i < size; ++i) {
+                Vet[i] = v.Vet[i];
+            }
+            
+        }
+
+        ~Vetor(){
+            delete [] Vet;
+
+        }
+
+};
