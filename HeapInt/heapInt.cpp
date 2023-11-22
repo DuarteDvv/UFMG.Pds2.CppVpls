b7de7350b3024b344c2e9debc517759ddb77b228
Luis Antonio Duarte Sousa <136333571+DuarteDvv@users.noreply.github.com>
2023-11-21 21:59:54 -0300

    Update heapInt.cpp

--- HeapInt/heapInt.cpp
+++ HeapInt/heapInt.cpp
@@ -1,1 +1,35 @@
+#include "heapi.hpp"
 
+HeapInt::HeapInt(){
+    this->inteiro = new int(0);
+
+}
+
+HeapInt::HeapInt(int &a){
+    this->inteiro = new int(a);
+
+}
+
+HeapInt::HeapInt(const HeapInt &a){
+    this& = a ;
+
+}
+
+HeapInt::~HeapInt(){
+    delete this->inteiro;
+
+}
+
+int HeapInt::getInt() const {
+    return *(this->inteiro);
+}
+
+void HeapInt::setInt(int a){
+    *(this->inteiro) = a;
+}
+
+HeapInt& HeapInt::operator = (const HeapInt &a){
+    this->setInt(a.getInt());
+    return *this;
+
+}
