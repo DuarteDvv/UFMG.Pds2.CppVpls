e76638e0930148fbe911e3207b8c5328aaea177e
Luis Antonio Duarte Sousa <136333571+DuarteDvv@users.noreply.github.com>
2023-11-23 10:25:05 -0300

    Create Reserva.hpp

---
+++ ExceptionHotel/Reserva.hpp
@@ -0,0 +1,27 @@
+#ifndef RESERVA_H
+#define RESERVA_H
+
+#define DIA_EM_SEGUNDOS 86400
+
+#define FORMATO_DATA "%d/%m/%Y"
+
+#include "ReservaExceptions.hpp"
+
+#include <iostream>
+#include <iomanip>
+
+class Reserva {
+private:
+    struct std::tm _data_entrada = {};
+    struct std::tm _data_saida = {};
+
+public:
+    Reserva(struct std::tm data_entrada, struct std::tm data_saida);
+
+    struct std::tm *get_data_entrada();
+    struct std::tm *get_data_saida();
+    int comparar(Reserva& other);
+    void print_info();
+};
+
+#endif
