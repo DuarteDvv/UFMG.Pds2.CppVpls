a32ce7302fc4a4927855ff14ba9497ed1695b8a4
DuarteDvv <luisduarte.dev@gmail.com>
2023-11-25 16:08:20 -0300

    att

--- ExceptionHotel/Hotel.cpp
+++ ExceptionHotel/Hotel.cpp
@@ -2,7 +2,7 @@
 
 Hotel::Hotel() {
     for (int i = 0; i < 6; i++) {
-        _quartos.push_back(new Quarto(i));
+        _quartos.push_back(nullptr);
     }
 }
 
@@ -17,10 +17,6 @@ std::vector<Quarto *> *Hotel::get_quartos() {
 }
 
 Quarto *Hotel::get_quarto_pelo_n(int n_quarto) {
-    if (n_quarto < 0 || n_quarto >= 6) {
-        throw hotel_excp::quarto_nao_existe(n_quarto);
-    }
-
     return _quartos[n_quarto];
 }
 
@@ -29,7 +25,16 @@ void Hotel::adiciona_reserva(int n_quarto, struct std::tm data_entrada, struct s
         throw hotel_excp::quarto_nao_existe(n_quarto);
     }
 
-    get_quarto_pelo_n(n_quarto)->adiciona_reserva(data_entrada, data_saida);
+    Quarto* Q = get_quarto_pelo_n(n_quarto);
+    if(Q == nullptr){
+        Q = new Quarto(n_quarto);
+        _quartos[n_quarto] = Q;
+        Q->adiciona_reserva(data_entrada, data_saida);
+        throw hotel_excp::quarto_nao_criado(n_quarto);
+    }else{
+        _quartos[n_quarto] = Q;
+        Q->adiciona_reserva(data_entrada, data_saida);
+    }
 }
 
 void Hotel::remove_reserva(int n_quarto, struct std::tm data_entrada) {
@@ -42,6 +47,9 @@ void Hotel::remove_reserva(int n_quarto, struct std::tm data_entrada) {
 
 void Hotel::print_info() {
     for (auto n : _quartos) {
-        n->print_info();
+        if(n != nullptr){
+            (n)->print_info();
+        }
     }
 }
+ 
